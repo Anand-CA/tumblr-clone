4131a8c180e004917834c75d3bff6d803841e6de
anand <anandca.an7@gmail.com>
2022-03-06 23:45:02 +0530

    google signin done

--- src/layout/Navbar/Navbar.js
+++ src/layout/Navbar/Navbar.js
@@ -1,24 +1,133 @@
-import { Input } from "@nextui-org/react";
+import {
+	Button,
+	Checkbox,
+	Input,
+	Modal,
+	Row,
+	Spacer,
+	Text
+} from "@nextui-org/react";
 import React from "react";
 import { Container, Left, Logo, Right, Wrapper } from "./Navbar.styled";
+import { HiLockClosed } from "react-icons/hi";
+import { FiMail } from "react-icons/fi";
+import { FcGoogle } from "react-icons/fc";
+import { useTheme } from "styled-components";
+import axios from "axios";
+import Link from "next/link";
+import { GoogleLogin } from "react-google-login";
 
 const Navbar = () => {
-	return (
-		<Wrapper>
-			<Container>
-				<Left>
-					<Logo src="/tumblr-logo.svg" alt="tumblr logo" />
-					<Input clearable placeholder="Type something..." />
-				</Left>
+	const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+	const [visible, setVisible] = React.useState(false);
+	const [showPass, setShowPass] = React.useState(false);
+	const open = () => setVisible(true);
+	const close = () => {
+		setVisible(false);
+	};
+	const theme = useTheme();
 
-				<Right>
-					<img src="/navbar/home.svg" alt="home" />
-					<img src="/navbar/explore.svg" alt="explore" />
-					<img src="/navbar/chat.svg" alt="chat" />
-					<img src="/navbar/avatar.svg" alt="avatar" />
-				</Right>
-			</Container>
-		</Wrapper>
+	function onGoogleSuccess(response) {
+		axios
+			.post("http://localhost:8000/api/v1/auth/google", {
+				tokenId: response.tokenId
+			})
+			.then(res => {
+				console.log(res);
+				setIsAuthenticated(true);
+				setVisible(false);
+				// store user in redux and persist user
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
+
+	function onGoogleFailure(response) {
+		console.log(response);
+	}
+	return (
+		<>
+			<Modal
+				closeButton
+				aria-labelledby="modal-title"
+				open={visible}
+				onClose={close}
+			>
+				<Modal.Header>
+					<Text id="modal-title" size={30}>
+						Login
+					</Text>
+				</Modal.Header>
+				<Modal.Body>
+					<Input
+						clearable
+						bordered
+						fullWidth
+						color="primary"
+						size="lg"
+						placeholder="Email"
+						contentLeft={<FiMail />}
+					/>
+					<Input
+						clearable
+						bordered
+						fullWidth
+						color="primary"
+						type={showPass ? "text" : "password"}
+						size="lg"
+						placeholder="Password"
+						contentLeft={<HiLockClosed />}
+					/>
+					<Button
+						css={{
+							background: theme.colors.primary
+						}}
+					>
+						Sign In
+					</Button>
+					<Text css={{ textAlign: "center" }}>OR</Text>
+					<Spacer y="$3" />
+					<GoogleLogin
+						clientId="43606210965-2cgmfflk9qhp485q39mtrg4gactu0veh.apps.googleusercontent.com"
+						buttonText="Login with google"
+						onSuccess={onGoogleSuccess}
+						onFailure={onGoogleFailure}
+					/>
+				</Modal.Body>
+				<Modal.Footer></Modal.Footer>
+			</Modal>
+			{/* ---------------------------------------------------- */}
+			<Wrapper>
+				<Container>
+					<Left>
+						<Logo src="/tumblr-logo.svg" alt="tumblr logo" />
+						<Input clearable placeholder="Type something..." />
+					</Left>
+
+					<Right>
+						{isAuthenticated ? (
+							<>
+								<img src="/navbar/home.svg" alt="home" />
+								<img src="/navbar/explore.svg" alt="explore" />
+								<img src="/navbar/chat.svg" alt="chat" />
+								<img src="/navbar/avatar.svg" alt="avatar" />
+							</>
+						) : (
+							<Button
+								css={{
+									background: theme.colors.primary
+								}}
+								onClick={open}
+								auto
+							>
+								Sign In
+							</Button>
+						)}
+					</Right>
+				</Container>
+			</Wrapper>
+		</>
 	);
 };
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import { createGlobalStyle } from "styled-components";
+import { useCallback, useEffect } from "react";
+import { socket } from "../utils/socketio";
+import { ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   /* box sizing */
@@ -37,15 +40,37 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+function intitialiseSocket() {
+	console.log("render");
+	socket.auth = {
+		userId: "568u6787"
+	};
+	socket.connect();
+}
+
 function MyApp({ Component, pageProps }) {
+	const memo = useCallback(() => {}, []);
+	useEffect(() => {
+		memo();
+	}, [memo]);
+
+	const theme = {
+		colors: {
+			primary: "#00b8ff",
+			secondary: "#001935"
+		}
+	};
+
 	return (
 		<>
 			<Head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 			</Head>
-			<GlobalStyle />
-			<Component {...pageProps} />
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<Component {...pageProps} />
+			</ThemeProvider>
 		</>
 	);
 }
